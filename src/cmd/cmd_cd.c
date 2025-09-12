40c348ace6eff385ac8caabb6369ea9a7b996ef2
Viet Nguyen <susinguyen.2025@gmail.com>
2025-09-12 16:51:49 +0300

    modify cd to work with cd ., cd ../

--- src/cmd/cmd_cd.c
+++ src/cmd/cmd_cd.c
@@ -12,36 +12,88 @@
 
 #include "minishell.h"
 
-static	int	cd_only(t_cmd *cmds);
-static	int	cd_back(t_cmd *cmds);
-static	int	cd_absolute(t_cmd *cmds, char **args);
+// static	int	cd_only(t_cmd *cmds);
+// static	int	cd_back(t_cmd *cmds);
+// static	int	cd_absolute(t_cmd *cmds, char **args);
+
+static	int	change_dir(t_cmd *cmds, char *next_wd);
 
 int exec_cd(t_cmd *cmds, char **args)
 {
+	char	*next_wd;
+
 	printf("Exec cd\n");
 	if (!cmds)
 		//return(error_msg(cmds, 1, "cmds"));
 		return(error_msg(1, "cmds"));
-	if (args[1] == NULL )
-	{
-		if (cd_only(cmds) != 0)
-			//return ((error_msg(cmds, 1, "cd error")));
-			return ((error_msg(1, "cd error")));
-	}
+	if (args[1] == NULL)
+		next_wd = find_var(cmds->envp, "HOME");
 	else if (ft_strcmp(args[1], "-") == 0)
 	{
-		if (cd_back(cmds) != 0)
-			return ((error_msg(1, "cd error")));
+		next_wd = find_var(cmds->envp, "OLDPWD");
+		// printf("Value of next wd: %s\n", next_wd);
 	}
-	else
+	else if (args[1] && ft_strcmp(args[1], "-") != 0)
+		next_wd = args[1];
+	if (!next_wd)
+		return (1);
+	if (change_dir(cmds, next_wd) == 1)
 	{
-		if (cd_absolute(cmds, args) != 0)
-	 		//return (error_msg(cmds, 1, "cd absolute wrong"));
-			return (error_msg(1, "cd absolute wrong"));
+		free(next_wd);
+		return (1);
 	}
+	// if (args[1] == NULL )
+	// {
+	// 	if (cd_only(cmds) != 0)
+	// 		//return ((error_msg(cmds, 1, "cd error")));
+	// 		return ((error_msg(1, "cd error")));
+	// }
+	// else if (ft_strcmp(args[1], "-") == 0)
+	// {
+	// 	if (cd_back(cmds) != 0)
+	// 		return ((error_msg(1, "cd error")));
+	// }
+	// else
+	// {
+	// 	if (cd_absolute(cmds, args) != 0)
+	//  		//return (error_msg(cmds, 1, "cd absolute wrong"));
+	// 		return (error_msg(1, "cd absolute wrong"));
+	// }
 	return (0);
 }
 
+static	int	change_dir(t_cmd *cmds, char *next_wd)
+{
+	char	*old_wd;
+
+	old_wd = getcwd(NULL, 0);
+	if (!old_wd)
+		return (1);
+	if (chdir(next_wd) != 0)
+	{
+		perror("cd");
+		free(old_wd);
+		return (1);
+	}
+	//printf("Change dir successful\n");
+	// printf("The value of next_wd before updating env: %s\n", next_wd);
+	next_wd = getcwd(NULL, 0); // update nextcwd
+	if (!next_wd)
+    {
+        free(old_wd);
+        return (1);
+    }
+	if (update_env(cmds, "OLDPWD", old_wd) != 0 || update_env(cmds, "PWD", next_wd) != 0)
+	{
+		free(old_wd);
+		free(next_wd);
+		return (1);
+	}
+	free(old_wd);
+	free(next_wd);
+	return (0);
+}
+/*
 static	int	cd_only(t_cmd *cmds)
 {
 	char	*home;
@@ -119,5 +171,4 @@ static	int	cd_absolute(t_cmd *cmds, char **args)
 	free(oldcwd);
 	return (0);
 }
-
-
+*/
--- src/cmd/envp_utility.c
+++ src/cmd/envp_utility.c
@@ -25,21 +25,22 @@ int	update_env(t_cmd *cmds, char *key, char *value)
 	i = 0;
 	while (cmds->envp[i])
 	{
-		if (ft_strncmp(cmds->envp[i], key, ft_strlen(key)) == 0)
+		if (ft_strncmp(cmds->envp[i], key, ft_strlen(key)) == 0 &&
+			cmds->envp[i][ft_strlen(key)] == '=')
 		{
 			// printf("Find the string: %s\n", cmds->envp[i]);
 			new_str = malloc(sizeof(char) * (ft_strlen(key) + ft_strlen(value) + 2));
 			if (!new_str)
 				return (1); 
 			ft_copy_str(new_str, key, value);
-			// printf("New string: %s\n", new_str);
+			// printf("Copy new string: %s\n", new_str);
 			free(cmds->envp[i]);
 			cmds->envp[i] = new_str;
 			return (0);
 		}
 		i++;
 	}
-	printf("Uppdate fail: key '%s' not found\n", key);
+	// printf("Uppdate fail: key '%s' not found\n", key);
 	return (1);
 }
 
