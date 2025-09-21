7919e100dbeae5538867e0ec09328590e68fff59
Cristian-Andrei Dohanici <cdohanic@c2r1p4.hive.fi>
2025-09-21 19:29:00 +0300

    realloc function

--- src/utils/memory_utils.c
+++ src/utils/memory_utils.c
@@ -72,3 +72,46 @@ void	free_split(char **split)
 	}
 	free(split);
 }
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*dest1;
+	unsigned char	*src1;
+	size_t			i;
+
+	if (n == 0)
+		return (dest);
+	dest1 = (unsigned char *)dest;
+	src1 = (unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		dest1[i] = src1[i];
+		i++;
+	}
+	return (dest);
+}
+
+void *ft_realloc(void *ptr, size_t old_size, size_t new_size)
+{
+	void *new_ptr;
+	size_t	copy_size;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+		return (malloc(new_size));
+	new_ptr = malloc(new_size);
+	if (!new_ptr)
+		return (NULL);
+	if (old_size < new_size)
+		copy_size = old_size;
+	else
+		copy_size = new_size;
+	ft_memcpy(new_ptr, ptr, copy_size);
+	free(ptr);
+	return (new_ptr);
+}
