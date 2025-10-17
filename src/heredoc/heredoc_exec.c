8c766faebc71d2845044346b70aff86df2ff5082
Viet Nguyen <vinguyen@c2r1p8.hive.fi>
2025-10-17 13:05:05 +0300

    modify heredoc folder for norminette

--- src/heredoc/heredoc_exec.c
+++ src/heredoc/heredoc_exec.c
@@ -6,42 +6,22 @@
 /*   By: vinguyen <vinguyen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 13:34:25 by vinguyen          #+#    #+#             */
-/*   Updated: 2025/09/22 16:00:24 by vinguyen         ###   ########.fr       */
+/*   Updated: 2025/10/17 13:04:22 by vinguyen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static	int		heredoc_read(t_cmd *cmds);
-static	void	child_pipe_heredoc(t_pipe_simple *pipex, t_cmd *cmds, char **args, char **env[]);
+static	int	heredoc_read(t_cmd *cmds);
+static	void	child_pipe_heredoc(t_pipe_simple *pipex, t_cmd *cmds,
+				char **args, char **env[]);
 static	void	parent_pipe_heredoc(t_pipe_simple *pipex, t_cmd *cmds);
 
 int	heredoc(t_cmd *cmds, char **env[], char **args, int args_count)
 {
-	// char	*input;
-	// char	*output;
 	t_pipe_simple	pipe_heredoc;
 
 	printf("Execute heredoc\n");
-	// output = ft_strdup("");
-	// while (1)
-	// {
-	// 	input = readline("> ");
-	// 	if (!input)
-	// 		break;
-	// 	if ((ft_strcmp(input, cmds->here_doc) == 0)
-	// 		&& (ft_strlen(input) == ft_strlen(cmds->here_doc)))
-	// 	{
-	// 		printf("Detect EOF\n");
-	// 		free(input);
-	// 		break;
-	// 	}
-	// 	if (join_and_free(&output, input) == 1)
-	// 		return (1);
-	// }
-	// if (cmds->here_doc_cont)
-	// 	free(cmds->here_doc_cont);
-	// cmds->here_doc_cont = output;
 	if (heredoc_read(cmds) == 1)
 		return (1);
 	if (!args)
@@ -55,11 +35,11 @@ int	heredoc(t_cmd *cmds, char **env[], char **args, int args_count)
 		init_pipe_heredoc(&pipe_heredoc);
 		if (open_pipe_heredoc(&pipe_heredoc) == 1)
 			return (1);
-    	if (pipe_heredoc.pid == 0) // child
+		if (pipe_heredoc.pid == 0)
 			child_pipe_heredoc(&pipe_heredoc, cmds, args, env);
-		else // parent
+		else
 			parent_pipe_heredoc(&pipe_heredoc, cmds);
-    }
+	}
 	if (cmds->here_doc)
 		cmds->here_doc = NULL;
 	// if (cmds->here_doc_cont)
@@ -67,11 +47,12 @@ int	heredoc(t_cmd *cmds, char **env[], char **args, int args_count)
 	return (0);
 }
 
-static	void	child_pipe_heredoc(t_pipe_simple *pipex, t_cmd *cmds, char **args, char **env[])
+static	void	child_pipe_heredoc(t_pipe_simple *pipex,
+		t_cmd *cmds, char **args, char **env[])
 {
 	char	*path;
-	
-    connect_pipe_heredoc(pipex);
+
+	connect_pipe_heredoc(pipex);
 	path = correct_path(args[0], *env);
 	if (path != NULL)
 	{
@@ -95,17 +76,18 @@ static	void	child_pipe_heredoc(t_pipe_simple *pipex, t_cmd *cmds, char **args, c
 
 static	void	parent_pipe_heredoc(t_pipe_simple *pipex, t_cmd *cmds)
 {
-	close(pipex->pipefd[0]); // close read end
-    write(pipex->pipefd[1], cmds->here_doc_cont, ft_strlen(cmds->here_doc_cont));
-    close(pipex->pipefd[1]); // EOF for child
-    waitpid(pipex->pid, NULL, 0);
+	close(pipex->pipefd[0]);// close read end
+	write(pipex->pipefd[1], cmds->here_doc_cont,
+		ft_strlen(cmds->here_doc_cont));
+	close(pipex->pipefd[1]);// EOF for child
+	waitpid(pipex->pid, NULL, 0);
 }
 
-static	int		heredoc_read(t_cmd *cmds)
+static	int	heredoc_read(t_cmd *cmds)
 {
 	char	*input;
 	char	*output;
-	
+
 	output = ft_strdup("");
 	while (1)
 	{
