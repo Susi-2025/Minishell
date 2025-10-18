c8cb6597943032ad8c0f0b8ec6e4643319d61ad0
Viet Nguyen <vinguyen@c2r1p8.hive.fi>
2025-10-18 18:53:54 +0300

    modify cd for jumping out from deleted folder

--- src/command/cmd_cd.c
+++ src/command/cmd_cd.c
@@ -6,7 +6,7 @@
 /*   By: vinguyen <vinguyen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/17 12:15:12 by vinguyen          #+#    #+#             */
-/*   Updated: 2025/10/16 16:52:48 by vinguyen         ###   ########.fr       */
+/*   Updated: 2025/10/18 18:48:13 by vinguyen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 static	int	change_dir(t_cmd *cmds, char *next_wd);
 static	int	free_2_mem(char *old_wd, char *next_wd, int exit_code);
+static	char	*get_parent_dir(char **envp);
 
 int	exec_cd(t_cmd *cmds, char **args)
 {
@@ -25,27 +26,51 @@ int	exec_cd(t_cmd *cmds, char **args)
 	if (args[1] == NULL)
 	{
 		next_wd = find_var(cmds->envp, "HOME");
-		return (error_string_cd("HOME", 1));
+		if (!next_wd)
+			return (error_string_cd("HOME", 1));
 	}
 	else if (ft_strcmp(args[1], "-") == 0)
 	{
 		next_wd = find_var(cmds->envp, "OLDPWD");
-		return (error_string_cd("OLDPWD", 1));
+		if (!next_wd)
+			return (error_string_cd("OLDPWD", 1));
 	}
-	else if (args[1] && ft_strcmp(args[1], "-") != 0)
+	else 
 		next_wd = args[1];
 	if (!next_wd)
 		return (1);
+	printf("Before changing dir: next_wd is %s\n", next_wd);
 	if (change_dir(cmds, next_wd) == 1)
 		return (1);
 	return (0);
 }
 
+static	char	*get_parent_dir(char **envp)
+{
+	char	*pwd;
+	char	*dup;
+	char	*par_pwd;
+
+	pwd = find_var(envp, "PWD");
+	if (!pwd)
+		return (NULL);
+	dup = ft_strdup(pwd);
+	if (!dup)
+		return (NULL);
+	par_pwd = ft_strjoin(dup, "/..");
+	free(dup);
+	if(!par_pwd)
+		return (NULL);
+	return (par_pwd);
+}
+
 static	int	change_dir(t_cmd *cmds, char *next_wd)
 {
 	char	*old_wd;
+	char	*temp_wd;
 
-	old_wd = getcwd(NULL, 0);
+	
+	old_wd = ft_strdup(find_var(cmds->envp, "PWD"));
 	if (!old_wd)
 		return (1);
 	if (chdir(next_wd) != 0)
@@ -53,11 +78,16 @@ static	int	change_dir(t_cmd *cmds, char *next_wd)
 		perror("cd");
 		return (free_2_mem(old_wd, NULL, 1));
 	}
-	next_wd = getcwd(NULL, 0);
-	if (!next_wd)
-		return (free_2_mem(old_wd, NULL, 1));
-	if (update_env(cmds, "OLDPWD", old_wd) != 0
-		|| update_env(cmds, "PWD", next_wd) != 0)
+	temp_wd = getcwd(NULL, 0);
+	if (!temp_wd)
+	{
+		printf("cd: error retrieving current directory: getcwd: cannot access parent directories: No such file or directory\n");
+    	temp_wd = get_parent_dir(cmds->envp);
+		if (!temp_wd)
+			return (free_2_mem(old_wd, NULL, 1));
+	}
+	next_wd = temp_wd;
+	if (update_env(cmds, "OLDPWD", old_wd) != 0 || update_env(cmds, "PWD", next_wd) != 0)
 		return (free_2_mem(old_wd, next_wd, 1));
 	return (free_2_mem(old_wd, next_wd, 0));
 }
